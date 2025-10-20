37a3fa9b8314c1edfce893034ddb3753c9bfccd5
Charbel Toumieh <charbel.toumieh@epfl.ch>
2025-10-20 08:25:59 +0200

    update

---
+++ mpc_controller_ros2/launch/mpc_omninxt.launch.py
@@ -0,0 +1,22 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_name = 'mpc_controller_ros2'
+    pkg_share = get_package_share_directory(pkg_name)
+    params_file = os.path.join(pkg_share, 'config', 'mpc_ominixt_params.yaml')
+
+    mpc_node = Node(
+        package=pkg_name,
+        executable='mpc_controller_node',   # Executable name stays the same
+        name='mpc_controller',               # Updated to match Node constructor name
+        output='screen',
+        emulate_tty=True,
+        parameters=[params_file]
+    )
+
+    return LaunchDescription([
+        mpc_node
+    ])
